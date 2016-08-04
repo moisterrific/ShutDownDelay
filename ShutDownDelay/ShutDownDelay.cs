c026ef442f20ea0c6927c0ff0ebe67e877a7cdd0
Enerdy <rodrigo.rente@live.com>
2016-08-04 01:20:16 +0100

    Add  and  optional params, change from an update hook to a one-second timer

--- ShutDownDelay/ShutDownDelay.cs
+++ ShutDownDelay/ShutDownDelay.cs
@@ -8,7 +8,7 @@ using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Newtonsoft.Json;
-using Thread = System.Threading.Thread;
+using System.Timers;
 
 namespace ShutDownDelay
 {
@@ -18,12 +18,12 @@ namespace ShutDownDelay
         private Config config = new Config();
         public static string configPath = Path.Combine(TShock.SavePath, "ShutDownDelayConfig.json");
 
-        private bool shutDownInProgress = false;
-        private bool shutDownPaused = false;
+        private bool shutDownInProgress => _timer.Enabled;
+        private bool shutDownPaused => !_timer.Enabled && _delay > 0;
 
-        private int oldDelay;
-
-        private DateTime lastCheck = DateTime.UtcNow;
+        private int _delay = 0;
+        private string _reason;
+        private Timer _timer;
 
         public override string Author
         {
@@ -66,7 +66,9 @@ namespace ShutDownDelay
         public override void Initialize()
         {
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
-            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
+
+            _timer = new Timer(1000);
+            _timer.Elapsed += timerUpdate;
         }
 
         protected override void Dispose(bool disposing)
@@ -74,9 +76,13 @@ namespace ShutDownDelay
             if (disposing)
             {
                 ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
-                ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
+
+                if (_timer.Enabled)
+                {
+                    _timer.Stop();
+                }
+                _timer.Elapsed -= timerUpdate;
             }
-            base.Dispose(disposing);
         }
         #endregion
 
@@ -87,26 +93,18 @@ namespace ShutDownDelay
             Commands.ChatCommands.Add(new Command("sdd.use", ShutDownCmd, "sddelay", "sdd"));
         }
 
-        private void OnUpdate(EventArgs args)
+        private void timerUpdate(object sender, ElapsedEventArgs e)
         {
-            if ((DateTime.UtcNow - lastCheck).TotalSeconds >= 1)
+            _delay--;
+            if (config.notifyIntervals.Contains(_delay))
             {
-                lastCheck = DateTime.UtcNow;
+                TSPlayer.All.SendInfoMessage("Server shutting down in {0} second{1}.", _delay, _delay > 1 ? "s" : "");
+            }
 
-                if (shutDownInProgress)
-                {
-                    config.delay--;
-
-                    if (config.notifyIntervals.Contains(config.delay))
-                    {
-                        TSPlayer.All.SendInfoMessage("Server shutting down in {0} second{1}.", config.delay.ToString(), config.delay > 1 ? "s" : "");
-                    }
-
-                    if (config.delay == 0)
-                    {
-                        TShock.Utils.StopServer(true, config.reason == "" ? "Server shutting down!" : config.reason);
-                    }
-                }
+            if (_delay == 0)
+            {
+                _timer.Stop();
+                TShock.Utils.StopServer(true, _reason == "" ? "Server shutting down!" : _reason);
             }
         }
         #endregion
@@ -120,6 +118,16 @@ namespace ShutDownDelay
                 return;
             }
 
+            /* Manipulate the reason at will, be it through the start, stop or continue command, in the event a reason
+             * other than the default one set in the config is required */
+            for (int i = 0; i < args.Parameters.Count; i++)
+            {
+                if (args.Parameters[i].StartsWith("-reason", StringComparison.OrdinalIgnoreCase) && ++i < args.Parameters.Count)
+                {
+                    _reason = String.Join(" ", args.Parameters.GetRange(i, args.Parameters.Count - i));
+                }
+            }
+
             switch (args.Parameters[0].ToLower())
             {
                 case "start":
@@ -131,9 +139,35 @@ namespace ShutDownDelay
                         }
                         else
                         {
-                            oldDelay = config.delay;
-                            shutDownInProgress = true;
-                            TSPlayer.All.SendSuccessMessage("The server will shut down in {0} second{1}. Reason: {2}", config.delay.ToString(), config.delay > 1 ? "s" : "", config.reason == "" ? "No reason specified." : config.reason);
+                            // Set custom delays on the fly with the '-delay' argument
+                            for (int i = 0; i < args.Parameters.Count; i++)
+                            {
+                                if (args.Parameters[i].StartsWith("-delay", StringComparison.OrdinalIgnoreCase) && ++i < args.Parameters.Count)
+                                {
+                                    int parsedDelay;
+                                    if (!Int32.TryParse(args.Parameters[i], out parsedDelay) || parsedDelay < 1)
+                                    {
+                                        args.Player.SendErrorMessage("Invalid delay!");
+                                        return;
+                                    }
+                                    _delay = parsedDelay;
+                                }
+                            }
+
+                            // If delay was not modified, use the configured one
+                            if (_delay == 0)
+                            {
+                                _delay = config.delay;
+                            }
+
+                            // In the event no '-reason' argument was passed, use the configured one
+                            if (String.IsNullOrEmpty(_reason))
+                            {
+                                _reason = config.reason;
+                            }
+
+                            _timer.Start();
+                            TSPlayer.All.SendSuccessMessage("The server will shut down in {0} second{1}. Reason: {2}", _delay, _delay > 1 ? "s" : "", _reason == "" ? "No reason specified." : _reason);
                         }
                     }
                     return;
@@ -151,8 +185,7 @@ namespace ShutDownDelay
                         }
                         else
                         {
-                            shutDownPaused = true;
-                            shutDownInProgress = false;
+                            _timer.Stop();
                             TSPlayer.All.SendSuccessMessage("The shutdown has been paused.");
                         }
                     }
@@ -166,8 +199,7 @@ namespace ShutDownDelay
                         }
                         else
                         {
-                            shutDownPaused = false;
-                            shutDownInProgress = true;
+                            _timer.Start();
                             TSPlayer.All.SendSuccessMessage("The shutdown has been continued.");
                         }
                     }
@@ -181,9 +213,8 @@ namespace ShutDownDelay
                         }
                         else
                         {
-                            config.delay = oldDelay;
-                            shutDownInProgress = false;
-                            shutDownPaused = false;
+                            _timer.Stop();
+                            _delay = 0;
                             TSPlayer.All.SendSuccessMessage("The shutdown has been canceled.");
                         }
                     }
